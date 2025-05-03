13093f7b1067593e40198c2bbe9e3ce6ca49b3c0
geno <genaro.flores.bustos1@gmail.com>
2025-05-03 12:31:23 -0400

    db added and display correct information of urls

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+import ShortUrl from "./models/shortUrl.js";
+
+
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.render("index");
+mongoose
+    .connect("mongodb://localhost/urlShortener",)
+    .then(() => console.log('✅ Conectado a MongoDB'))
+    .catch(err => {
+        console.error('❌ Error al conectar a MongoDB:', err);
+        process.exit(1);
+    });
+
+app.get("/", async (req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render("index", { shortUrls: shortUrls });
+});
+
+
+
+app.post("/shortUrls", async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect("/");
 });
 
 app.listen(process.env.PORT || 5000);
